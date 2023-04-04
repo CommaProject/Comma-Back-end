2a6417cad8d28a13fe2ae30c63c738b84ab77e92
5tr1ker <follow2272@naver.com>
2023-04-04 17:10:57 +0900

    refactor : OAuth2.0 Provider 파일을 properties로 이동
    OAuth2.0 Provider 파일을 properties로 이동

--- src/main/java/com/team/comma/SecurityConfig.java
+++ src/main/java/com/team/comma/SecurityConfig.java
@@ -1,24 +1,13 @@
 package com.team.comma;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.team.comma.oauth.OAuth2Provider;
 import com.team.comma.security.JwtAuthenticationFilter;
 import com.team.comma.security.JwtTokenProvider;
 
@@ -32,13 +21,9 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	final private JwtTokenProvider jwtTokenProvider;
-	final private List<String> clients = Arrays.asList("google", "kakao", "naver");
-	final private Environment env;
-	final private String CLIENT_PROPERTY_KEY= "spring.security.oauth2.client.registration.";
 	
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http.csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/security/**").hasRole("USER")
@@ -58,55 +43,5 @@ public class SecurityConfig {
 				UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
-    }
-    
-    @Bean
-    public OAuth2AuthorizedClientService authorizedClientService() {
-        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
-    }
-    
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        List<ClientRegistration> registrations = clients.stream()
-                .map(c -> getRegistration(c))
-                .filter(registration -> registration != null)
-                .collect(Collectors.toList());
-        return new InMemoryClientRegistrationRepository(registrations);
-    }
-    
-    private ClientRegistration getRegistration(String client){
-        // API Client Id 불러오기
-        String clientId = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-id");
-
-        // API Client Id 값이 존재하는지 확인하기
-        if (clientId == null) {
-            return null;
-        }
-
-        // API Client Secret 불러오기
-        String clientSecret = env.getProperty(
-                CLIENT_PROPERTY_KEY + client + ".client-secret");
-
-        if (client.equals("google")) {
-            return OAuth2Provider.GOOGLE.getBuilder(client)
-                    .clientId(clientId)
-                    .clientSecret(clientSecret)
-                    .build();
-        }
-
-        if (client.equals("kakao")) {
-            return OAuth2Provider.KAKAO.getBuilder(client)
-                    .clientId(clientId)
-                    .build();
-        }
-
-        if (client.equals("naver")) {
-            return OAuth2Provider.NAVER.getBuilder(client)
-                    .clientId(clientId)
-                    .clientSecret(clientSecret)
-                    .build();
-        }
-
-        return null;
     }
 }
