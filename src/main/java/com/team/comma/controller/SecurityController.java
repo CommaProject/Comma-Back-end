6b613f3902a27f1db08c77914cfe8e9a007d3163
5tr1ker <follow2272@naver.com>
2023-04-05 16:03:09 +0900

    feat: 로그아웃 메세지 추가
    로그아웃 메세지 추가

--- src/main/java/com/team/comma/controller/SecurityController.java
+++ src/main/java/com/team/comma/controller/SecurityController.java
@@ -16,7 +16,7 @@ public class SecurityController {
 	final private JwtService jwtService;
 
 	@GetMapping(value = "/authentication/denied")
-	public MessageResponse authenticationDenied(@CookieValue(name = "refreshToken" , required = false) String authorization) {
+	public MessageResponse informAuthenticationDenied(@CookieValue(name = "refreshToken" , required = false) String authorization) {
 		if(authorization == null) {
 			return MessageResponse.builder()
 					.code(-1)
@@ -29,10 +29,18 @@ public class SecurityController {
 	}
 	
 	@GetMapping(value = "/authorization/denied")
-	public MessageResponse authorizationDenied() {
+	public MessageResponse informAuthorizationDenied() {
 		return MessageResponse.builder()
 				.code(-1)
 				.message("인가되지 않은 사용자입니다.")
+				.build();
+	}
+	
+	@GetMapping(value = "/logout/message")
+	public MessageResponse logoutMessage() {
+		return MessageResponse.builder()
+				.code(1)
+				.message("로그아웃이 성공적으로 되었습니다.")
 				.build();
 	}
 	
