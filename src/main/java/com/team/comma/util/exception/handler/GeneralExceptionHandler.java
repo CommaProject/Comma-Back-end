af081746825b5040a63565009d82d76a17e9d2c2
5tr1ker <tjseocld@naver.com>
2023-05-15 16:12:23 +0900

    feat: 누락된 코드 추가
    feat: 누락된 코드 추가

--- src/main/java/com/team/comma/util/exception/handler/GeneralExceptionHandler.java
+++ src/main/java/com/team/comma/util/exception/handler/GeneralExceptionHandler.java
@@ -10,11 +10,14 @@ import com.team.comma.spotify.search.exception.TokenExpirationException;
 import com.team.comma.util.auth.exception.OAuth2EmailNotFoundException;
 import com.team.comma.util.jwt.exception.TokenForgeryException;
 import com.team.comma.util.s3.exception.S3Exception;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import se.michaelthelin.spotify.exceptions.detailed.UnauthorizedException;
 
 import javax.security.auth.login.AccountException;
@@ -25,11 +28,8 @@ import static com.team.comma.common.constant.ResponseCodeEnum.*;
 @RestControllerAdvice
 public class GeneralExceptionHandler {
 
-    /*
-     * 토큰 변조 , 사용자를 찾을 수 없을 때 , 사용자가 이미 존재하거나 정보가 일치하지 않을 때
-     */
-    @ExceptionHandler({UsernameNotFoundException.class, AccountException.class , S3Exception.class , FollowingException.class ,
-            FavoriteArtistException.class })
+    @ExceptionHandler({UsernameNotFoundException.class, AccountException.class , S3Exception.class
+            , FollowingException.class , FavoriteArtistException.class })
     public ResponseEntity<MessageResponse> handleBadRequest(Exception e) {
         MessageResponse message = MessageResponse.of(SIMPLE_REQUEST_FAILURE.getCode() ,
             e.getMessage());
@@ -37,9 +37,6 @@ public class GeneralExceptionHandler {
         return ResponseEntity.badRequest().body(message);
     }
 
-    /*
-        토큰 변조
-     */
     @ExceptionHandler(TokenForgeryException.class)
     public ResponseEntity<MessageResponse> handleForbiddenRequest(Exception e) {
         MessageResponse message = MessageResponse.of(AUTHORIZATION_ERROR.getCode(),
@@ -48,9 +45,6 @@ public class GeneralExceptionHandler {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(message);
     }
 
-    /*
-     * OAuth2.0 존재하지 않은 이메일
-     */
     @ExceptionHandler({OAuth2EmailNotFoundException.class})
     public ResponseEntity<MessageResponse> handleAccountException(Exception e) {
         MessageResponse message = MessageResponse.of(OAUTH_NO_EXISTENT_EMAIL.getCode() ,
@@ -59,9 +53,6 @@ public class GeneralExceptionHandler {
         return ResponseEntity.badRequest().body(message);
     }
 
-    /*
-     *  Spotify 예외
-     */
     @ExceptionHandler({SpotifyException.class, UnauthorizedException.class})
     public ResponseEntity<MessageResponse> handleSpotifyException(Exception e) {
         MessageResponse message = MessageResponse.of(SPOTIFY_FAILURE.getCode() , e.getMessage());
@@ -69,9 +60,6 @@ public class GeneralExceptionHandler {
         return ResponseEntity.internalServerError().body(message);
     }
 
-    /*
-        RefreshToken 만료
-     */
     @ExceptionHandler({TokenExpirationException.class})
     public ResponseEntity<MessageResponse> handleExpireTokenException(Exception e) {
         MessageResponse message = MessageResponse.of(REFRESH_TOKEN_EXPIRED.getCode() ,
@@ -80,9 +68,6 @@ public class GeneralExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
     }
 
-    /*
-        알람 설정 변경 시 플레이리스트 찾을 수 없음
-     */
     @ExceptionHandler({PlaylistException.class})
     public ResponseEntity<MessageResponse> handlePlaylistNotFoundException(Exception e){
         MessageResponse message = MessageResponse.of(ResponseCode.ALARM_UPDATE_FAILURE,
@@ -90,5 +75,37 @@ public class GeneralExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
 
+    }
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<MessageResponse> handleMethodArgumentTypeMismatchException(Exception e) {
+        MessageResponse message = MessageResponse.of(
+                REQUEST_TYPE_MISMATCH.getCode(),
+                REQUEST_TYPE_MISMATCH.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(message);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<MessageResponse> handleEntityNotFoundException(
+            EntityNotFoundException e) {
+        MessageResponse message = MessageResponse.of(
+                REQUEST_ENTITY_NOT_FOUND.getCode(),
+                REQUEST_ENTITY_NOT_FOUND.getMessage()
+        );
+        return ResponseEntity.badRequest().body(message);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        MessageResponse message = MessageResponse.of(
+                REQUEST_TYPE_MISMATCH.getCode(),
+                REQUEST_TYPE_MISMATCH.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(message);
     }
 }
