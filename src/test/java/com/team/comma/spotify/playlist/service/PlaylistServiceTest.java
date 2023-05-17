474ee63aa4bbe56bf31256dd20a4b9407a193c11
given02 <votmdnjem_id@naver.com>
2023-05-17 13:34:32 +0900

    feature: 커밋 테스트(rollback)

--- src/test/java/com/team/comma/spotify/playlist/service/PlaylistServiceTest.java
+++ src/test/java/com/team/comma/spotify/playlist/service/PlaylistServiceTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-// 커밋 테스트
+
 @ExtendWith(MockitoExtension.class)
 class PlaylistServiceTest {
 
