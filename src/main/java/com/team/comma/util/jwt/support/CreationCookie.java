507b97cac92ab62208be120bb6d81cd6ed142e85
5tr1ker <tjseocld@naver.com>
2023-07-26 00:37:58 +0900

    fix: 쿠키 도메인 데이터 변경
    fix: 쿠키 도메인 데이터 변경

--- src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
+++ src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
@@ -7,27 +7,28 @@ import org.springframework.http.ResponseCookie;
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class CreationCookie {
 
-    @Value("http://localhost:3000")
-    private static String DOMAIN_URL;
+    private static String DOMAIN_URL = "localhost:3000";
 
     public static ResponseCookie createResponseAccessToken(String cookieName) {
         return ResponseCookie.from("accessToken", cookieName)
-            .httpOnly(true)
-            // .secure(true)
-            .path("/")
-            // .sameSite("None")
-            .maxAge(30 * 60 * 1000L)
-            .build();
+                .httpOnly(true)
+                // .secure(true)
+                .path("/")
+                // .sameSite("None")
+                .maxAge(30 * 60 * 1000L)
+                .domain(DOMAIN_URL)
+                .build();
     }
 
     public static ResponseCookie createResponseRefreshToken(String cookieName) {
         return ResponseCookie.from("refreshToken", cookieName)
-            .httpOnly(true)
-            // .secure(true)
-            .path("/")
-            // .sameSite("None")
-            .maxAge(14 * 24 * 60 * 60 * 1000L)
-            .build();
+                .httpOnly(true)
+                // .secure(true)
+                .path("/")
+                // .sameSite("None")
+                .domain(DOMAIN_URL)
+                .maxAge(14 * 24 * 60 * 60 * 1000L)
+                .build();
     }
 
 }
