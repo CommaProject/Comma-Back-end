c3f0101d91ccbccab6e667a18d6e7a2b104ccd07
given02 <votmdnjem_id@naver.com>
2023-07-25 13:36:03 +0900

    Update CreationCookie.java

--- src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
+++ src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
@@ -12,7 +12,7 @@ public class CreationCookie {
 
     public static ResponseCookie createResponseAccessToken(String cookieName) {
         return ResponseCookie.from("accessToken", cookieName)
-            // .httpOnly(true)
+            .httpOnly(true)
             .secure(true)
             .path("/")
             .sameSite("None")
@@ -23,7 +23,7 @@ public class CreationCookie {
 
     public static ResponseCookie createResponseRefreshToken(String cookieName) {
         return ResponseCookie.from("refreshToken", cookieName)
-            // .httpOnly(true)
+            .httpOnly(true)
             .secure(true)
             .path("/")
             .sameSite("None")
