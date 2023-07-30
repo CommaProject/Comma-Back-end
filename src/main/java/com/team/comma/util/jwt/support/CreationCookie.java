dc3bafb8f07ef441d2196a048f97399062cbd2ea
5tr1ker <tjseocld@naver.com>
2023-07-30 23:05:46 +0900

    fix: 쿠키가 등록되지 않은 버그 수정
    fix: 쿠키가 등록되지 않은 버그 수정

--- src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
+++ src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
@@ -1,21 +1,22 @@
 package com.team.comma.util.jwt.support;
 
+import com.team.comma.util.security.domain.Token;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.ResponseCookie;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class CreationCookie {
-  
-    @Value("${server.url}")
-    private static String DOMAIN_URL;
 
     public static ResponseCookie createResponseAccessToken(String cookieName) {
         return ResponseCookie.from("accessToken", cookieName)
                 .httpOnly(true)
-                // .secure(true)
+                //.secure(true)
                 .path("/")
-                // .sameSite("None")
+                .domain("localhost")
+                //.sameSite(Cookie.SameSite.NONE.name())
                 .maxAge(30 * 60 * 1000L)
                 .build();
     }
@@ -23,11 +24,17 @@ public class CreationCookie {
     public static ResponseCookie createResponseRefreshToken(String cookieName) {
         return ResponseCookie.from("refreshToken", cookieName)
                 .httpOnly(true)
-                // .secure(true)
+                //.secure(true)
                 .path("/")
-                // .sameSite("None")
+                .domain("localhost")
+                //.sameSite(Cookie.SameSite.NONE.name())
                 .maxAge(14 * 24 * 60 * 60 * 1000L)
                 .build();
+    }
+
+    public static void setCookieFromJwt(HttpServletResponse response , Token token) {
+        response.addHeader("Set-Cookie" , createResponseAccessToken(token.getAccessToken()).toString());
+        response.addHeader("Set-Cookie" , createResponseRefreshToken(token.getRefreshToken()).toString());
     }
 
 }
