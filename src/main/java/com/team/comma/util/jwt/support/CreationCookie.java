64c33e5051fddaa315ac94103364ce91321e4d77
given02 <votmdnjem_id@naver.com>
2023-08-04 20:33:04 +0900

    Update CreationCookie.java

--- src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
+++ src/main/java/com/team/comma/util/jwt/support/CreationCookie.java
@@ -15,7 +15,7 @@ public class CreationCookie {
                 .httpOnly(true)
                 .secure(true)
                 .path("/")
-                .domain("localhost:3000")
+                .domain("localhost")
                 .sameSite(Cookie.SameSite.NONE.name())
                 .maxAge(30 * 60 * 1000L)
                 .build();
@@ -26,7 +26,7 @@ public class CreationCookie {
                 .httpOnly(true)
                 .secure(true)
                 .path("/")
-                .domain("localhost:3000")
+                .domain("localhost")
                 .sameSite(Cookie.SameSite.NONE.name())
                 .maxAge(14 * 24 * 60 * 60 * 1000L)
                 .build();
