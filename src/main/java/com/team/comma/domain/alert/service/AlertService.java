aea516b2413c8c575f7fd62ba001b73bdf10c6f2
5tr1ker <tjseocld@naver.com>
2023-09-15 21:35:34 +0900

    refactor: 사용하지 않는 import 제거
    refactor: 사용하지 않는 import 제거

--- src/main/java/com/team/comma/domain/alert/service/AlertService.java
+++ src/main/java/com/team/comma/domain/alert/service/AlertService.java
@@ -1,8 +1,6 @@
 package com.team.comma.domain.alert.service;
 
 import com.team.comma.domain.alert.dto.AlertResponse;
-import com.team.comma.domain.playlist.playlist.domain.Playlist;
-import com.team.comma.domain.playlist.playlist.dto.PlaylistResponse;
 import com.team.comma.domain.playlist.playlist.repository.PlaylistRepository;
 import com.team.comma.domain.user.user.domain.User;
 import com.team.comma.domain.user.user.exception.UserException;
